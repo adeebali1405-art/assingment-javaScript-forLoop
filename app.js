13537e53c8d34a636f73acdeee650f2cb5f9ad14
adeebali1405-art <adeebali1405@gmail.com>
2026-07-29 16:54:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+
+
+// for (var cointing = 1;cointing  <=10; cointing++){
+//     console.log(cointing)
+// }
+
+
+// for (var rversCointing =10;rversCointing>0;rversCointing-- ){
+//     console.log(rversCointing)
+// }
+
+
+// for (var messageDisplay=0;messageDisplay<5;messageDisplay++){
+//     console.log('Hello, JavaScript')
+// }
+
+// var fruits = ["Apple", "Banana", "Mango", "Orange"];
+// for (var fruitsNames = 0;fruitsNames<fruits.length;fruitsNames++){
+//     console.log(fruits[fruitsNames])
+// }
+
+// var marks = [50, 60, 70, 80];
+// var total = 0
+// for(var marksSum=0;marksSum<marks.length;marksSum++){
+// total=total + marks[marksSum]
+// }
+// console.log(total)
+// var avg = total/marks.length;
+// console.log(avg)
+
+// var colors = ["Red", "Green", "Blue", "Yellow"];
+// for (var colorsRvers = colors.length;colorsRvers>=0;colorsRvers--){
+//     console.log(colors[colorsRvers])
+// }
+
+// var numbers = [5, 10, 15, 20];
+// for (var i = 0; i < numbers.length ; i ++){
+//     console.log("Index"+i+ " : " +numbers[i])
+// }
+
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+var count = 0;
+for(var i = 0; i<cities.length;i++){
+    count=count +1
+}
+console.log('Total Element' + ' ' + cities.length)
+
+
+// var numbers = [2, 5, 8, 11, 14, 17, 20];
+// for(var even = 0;even<numbers.length;even++){
+//     if(numbers[even] %2 === 0){
+//      console.log(numbers[even])
+//     }
+// }
